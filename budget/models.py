96b30008f81ae3341d59d5e0865aee9971b41938
Bryn Portella <brynportella@gmail.com>
2020-08-20 08:09:03 -0400

    initial setup

---
+++ budget/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+# class BudgetExpense(models.Models):
+# 	amount = models.DecimalField()
+# 	start_date = models.DateTimeField()
+# 	end_date = models.DateTimeField()
+# 	recurrence= 
