e624e1bf0e36a588056369d24619167d4118795c
Bryn Portella <brynportella@gmail.com>
2020-08-24 00:04:47 -0400

    css changes more html

--- budgetingapp/urls.py
+++ budgetingapp/urls.py
@@ -21,9 +21,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'), 
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += [path('admin/', admin.site.urls)]
